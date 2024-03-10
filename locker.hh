f7aafa10e372d39b135108203c260f1dafb6c652
HOLIC0105 <3485995896@qq.com>
2024-03-10 22:31:00 +0800

    main

--- locker.hh
+++ locker.hh
@@ -2,17 +2,62 @@
 #define LOCKER_H
 
 #include <pthread.h>
-#include <vector>
-#include <sys/socket.h>
-class locker{
+#include <exception>
+
+class Locker{
   public:
-    locker() {
-      if(pthread_mutex_in)
+    Locker() {
+      if(pthread_mutex_init(&mutex_, NULL) != 0) {
+        throw std::exception();
+      }
     }
 
+    ~Locker() {
+      pthread_mutex_destroy(&mutex_);
+    }
+
+    bool Lock() {
+      return pthread_mutex_lock(&mutex_) == 0;
+    }
+
+    bool UnLock() {
+      return pthread_mutex_unlock(&mutex——) == 0;
+    }
+
+    pthread_mutex_t * Get() {
+      return &mutex_;
+    }
+
+
   private:
-    pthread_mutex_t mutex_ ;
+    pthread_mutex_t mutex_;
 
 };
+
+class Cond{
+  public:
+  Cond() {
+    if(pthread_cond_init(&cond_, NULL) != 0) {
+      throw std::exception();
+    }
+  }
+
+  ~Cond() {
+    pthread_cond_destroy(&cond_);
+  }
+
+  bool Wait(pthread_mutex_t * mutex) {
+    return pthread_cond_wait(&cond_, mutex) == 0;
+  }
+
+  bool TimeWait(pthread_mutex_t * mutex, timespec *tim) {
+    return pthread_cond_timedwait(&cond_, mutex, tim);
+  }
+
+
+  private:
+    pthread_cond_t cond_;
+}
+
 
 #endif // LOCKER_H
